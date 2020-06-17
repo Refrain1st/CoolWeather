290a576ff255ed29d8b8a2fdca3aafd099b7f67b
Refrain1st <1542689661@qq.com>
2020-06-17 15:15:33 +0000

    comme

--- app/src/androidTest/java/com/coolweather/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/coolweather/android/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.coolweather.android", appContext.getPackageName());
+        assertEquals("com.coolweather.android", appContext.getPackageName());//nihaoi
     }
 }
